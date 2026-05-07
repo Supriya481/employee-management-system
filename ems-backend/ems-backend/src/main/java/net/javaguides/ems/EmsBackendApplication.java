774b5b228732c3406893b582ac5347e9f7a50383
Supriya481 <pasupuletisupriya2002@gmail.com>
2026-05-07 14:35:50 +0530

    Enable JPA repositories in EmsBackendApplication
    Added @EnableJpaRepositories annotation to enable JPA repositories.

--- ems-backend/ems-backend/src/main/java/net/javaguides/ems/EmsBackendApplication.java
+++ ems-backend/ems-backend/src/main/java/net/javaguides/ems/EmsBackendApplication.java
@@ -2,12 +2,13 @@ package net.javaguides.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "net.javaguides.ems.repository")
 public class EmsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmsBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmsBackendApplication.class, args);
+    }
 }
