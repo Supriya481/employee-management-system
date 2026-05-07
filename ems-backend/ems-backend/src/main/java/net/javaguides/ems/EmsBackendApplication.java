2d41fa9dc5706f0132f32149b9d2c87d790d9d7a
Supriya481 <pasupuletisupriya2002@gmail.com>
2026-05-07 14:43:44 +0530

    Exclude DataSource and Hibernate JPA auto-configurations

--- ems-backend/ems-backend/src/main/java/net/javaguides/ems/EmsBackendApplication.java
+++ ems-backend/ems-backend/src/main/java/net/javaguides/ems/EmsBackendApplication.java
@@ -2,10 +2,15 @@ package net.javaguides.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
-@EnableJpaRepositories(basePackages = "net.javaguides.ems.repository")
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
 public class EmsBackendApplication {
 
     public static void main(String[] args) {
